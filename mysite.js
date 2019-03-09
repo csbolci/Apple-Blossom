40eb77d71fa2b9a9ec90749a4bebeba0afeed0d3
csbolci <42472792+csbolci@users.noreply.github.com>
2019-03-09 10:36:28 -0500

    Add files via upload

---
+++ mysite.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $(".program").mouseover(function(){
+        $(".program").css("background-color", "#F1C77C");
+    });
+    $(".program").mouseout(function(){
+        $(".program").css("background-color", "white");
+    });
+});
+$(document).ready(function(){
+    $(".design").mouseover(function(){
+        $(".design").css("background-color", "#EED1F5");
+    });
+    $(".design").mouseout(function(){
+        $(".design").css("background-color", "white");
+    });
+});
+$(document).ready(function(){
+    $(".illustration").mouseover(function(){
+        $(".illustration").css("background-color", "#666");
+    });
+    $(".illustration").mouseout(function(){
+        $(".illustration").css("background-color", "white");
+    });
+});
+$(document).ready(function(){
+    $(".etc").mouseover(function(){
+        $(".etc").css("background-color", "#DEFFB5");
+    });
+    $(".etc").mouseout(function(){
+        $(".etc").css("background-color", "white");
+    });
+});
